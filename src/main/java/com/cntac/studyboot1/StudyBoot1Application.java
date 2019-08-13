9c4171e28af5be57ee507d72b2830efe8527fee0
li9322 <1172892394@qq.com>
2019-08-13 16:52:00 +0800

    boot init

---
+++ src/main/java/com/cntac/studyboot1/StudyBoot1Application.java
@@ -0,0 +1,13 @@
+package com.cntac.studyboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBoot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyBoot1Application.class, args);
+    }
+
+}
